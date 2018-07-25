a82ef9bb904681fcf6d45e2412567ccd9ad4f3d8
Sergey Shpadyrev <sergeyshpadyrev@gmail.com>
2018-07-25 15:01:11 +0300

    Add more examples

--- example/05_replace/App.js
+++ example/05_replace/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Router from 'react-native-easy-router'
-import { Text, View } from 'react-native'
+import { Text, TextInput, View } from 'react-native'
 
 const Screen = ({ children, color }) => (
   <View
@@ -26,7 +26,15 @@ const Screen1 = ({ router }) => (
 const Screen2 = ({ router }) => (
   <Screen color="pink">
     <Text onPress={() => router.pop()}>Go back to screen 1 {/* Stack will be [Screen1] */}</Text>
+    <Text onPress={() => router.replace.Screen3()}>Replace with screen 3 {/* Stack will be [Screen1, Screen3] */}</Text>
   </Screen>
 )
 
-export default () => <Router routes={{ Screen1, Screen2 }} initialRoute="Screen1" />
+const Screen3 = ({ router }) => (
+  <Screen color="yellow">
+    <Text onPress={() => router.pop()}>Go back to screen 1 {/* Stack will be [Screen1] */}</Text>
+    <Text onPress={() => router.replace.Screen2()}>Replace with screen 2 {/* Stack will be [Screen1, Screen2] */}</Text>
+  </Screen>
+)
+
+export default () => <Router routes={{ Screen1, Screen2, Screen3 }} initialRoute="Screen1" />
