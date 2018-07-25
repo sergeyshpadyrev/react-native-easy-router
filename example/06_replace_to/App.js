4338d4bd72e5617ef0a08d865e9171411839aefb
Sergey Shpadyrev <sergeyshpadyrev@gmail.com>
2018-07-25 15:41:56 +0300

    Add more examples

---
+++ example/06_replace_to/App.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import Router from 'react-native-easy-router'
+import { Text, TextInput, View } from 'react-native'
+
+const Screen = ({ children, color }) => (
+  <View
+    style={{
+      alignItems: 'center',
+      backgroundColor: color,
+      display: 'flex',
+      flex: 1,
+      flexDirection: 'column',
+      justifyContent: 'space-around'
+    }}
+  >
+    {children}
+  </View>
+)
+
+const Screen1 = ({ router }) => (
+  <Screen color="white">
+    <Text onPress={() => router.push.Screen2()}>Go to screen 2 {/* Stack will be [Screen1, Screen2] */}</Text>
+  </Screen>
+)
+
+const Screen2 = ({ router }) => (
+  <Screen color="pink">
+    <Text onPress={() => router.push.Screen3()}>Go to screen 3 {/* Stack will be [Screen1, Screen2, Screen3] */}</Text>
+  </Screen>
+)
+
+const Screen3 = ({ router }) => (
+  <Screen color="yellow">
+    <Text onPress={() => router.stack[1].replace.Screen4()}>
+      Replace screen 1 with screen 4 {/* Stack will be [Screen1, Screen4] */}
+    </Text>
+  </Screen>
+)
+
+const Screen4 = ({ router }) => (
+  <Screen color="orange">
+    <Text onPress={() => router.replace.Screen2()}>Replace with screen 2 {/* Stack will be [Screen1, Screen2] */}</Text>
+  </Screen>
+)
+
+export default () => <Router routes={{ Screen1, Screen2, Screen3, Screen4 }} initialRoute="Screen1" />
