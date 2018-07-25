43186b8142356a2343c4d627badeaff332eeffcc
Sergey Shpadyrev <sergeyshpadyrev@gmail.com>
2018-07-25 23:50:11 +0300

    Start refactoring methods

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import Actions from './actions'
 import * as Animatable from 'react-native-animatable-promise'
 import Animation from './animation'
 import Hardware from './hardware'
+import Methods from './methods'
 import React from 'react'
 import styles from './styles'
 import { View } from 'react-native'
@@ -10,39 +11,7 @@ class Router extends React.Component {
   actions = new Actions()
   state = { stack: [] }
 
-  forAllRoutes = mapper => Object.assign(...Object.keys(this.props.routes).map(route => ({ [route]: mapper(route) })))
-  router = {
-    pop: animation =>
-      this.actions.add(onFinish => {
-        if (this.state.stack.length === 0) return
-        animation = Animation.withDefault(animation)
-
-        this.state.stack[this.state.stack.length - 1].reference
-          .transitionTo(Animation.start(animation.type), animation.duration, animation.easing)
-          .then(() => this.setState({ stack: this.state.stack.slice(0, -1) }, onFinish))
-      }),
-    push: this.forAllRoutes(route => (params, animation) =>
-      this.actions.add(onFinish => this.addScreen(route, params, animation, onFinish))
-    ),
-    replace: this.forAllRoutes(route => (params, animation) =>
-      this.actions.add(onFinish => {
-        const removeReplacedScreen = () => {
-          const cut = array => [...array.slice(0, -2), array[array.length - 1]]
-          this.setState({ stack: cut(this.state.stack) }, onFinish)
-        }
-        this.addScreen(route, params, animation, removeReplacedScreen, 1)
-      })
-    ),
-    reset: this.forAllRoutes(route => (params, animation) =>
-      this.actions.add(onFinish => {
-        const removeAllScreens = () => {
-          const cut = array => [array[array.length - 1]]
-          this.setState({ stack: cut(this.state.stack) }, onFinish)
-        }
-        this.addScreen(route, params, animation, removeAllScreens, this.state.stack.length)
-      })
-    )
-  }
+  router = new Methods(this)
   hardware = new Hardware(this.router, this.props.disableHardwareBack)
 
   addScreen = (route, params, animation, onActionFinished, idShift = 0) => {
@@ -92,9 +61,6 @@ class Router extends React.Component {
   }
 
   componentWillMount = () => {
-    const stackGetter = { get: () => this.state.stack.map(route => route.settings) }
-    Object.defineProperty(this.router, 'stack', stackGetter)
-
     this.addScreen(this.props.initialRoute, {}, { type: 'none' })
     this.hardware.subscribe()
 
---
+++ src/methods.js
@@ -0,0 +1,42 @@
+export default class Router {
+  constructor(router) {
+    Object.defineProperty(this, 'stack', { get: () => router.state.stack.map(route => route.settings) })
+
+    const forAllRoutes = mapper =>
+      Object.assign(...Object.keys(router.props.routes).map(route => ({ [route]: mapper(route) })))
+
+    this.pop = animation =>
+      router.actions.add(onFinish => {
+        if (router.state.stack.length === 0) return
+        animation = Animation.withDefault(animation)
+
+        router.state.stack[router.state.stack.length - 1].reference
+          .transitionTo(Animation.start(animation.type), animation.duration, animation.easing)
+          .then(() => router.setState({ stack: router.state.stack.slice(0, -1) }, onFinish))
+      })
+
+    this.push = forAllRoutes(route => (params, animation) =>
+      router.actions.add(onFinish => router.addScreen(route, params, animation, onFinish))
+    )
+
+    this.replace = forAllRoutes(route => (params, animation) =>
+      router.actions.add(onFinish => {
+        const removeReplacedScreen = () => {
+          const cut = array => [...array.slice(0, -2), array[array.length - 1]]
+          router.setState({ stack: cut(router.state.stack) }, onFinish)
+        }
+        router.addScreen(route, params, animation, removeReplacedScreen, 1)
+      })
+    )
+
+    this.reset = forAllRoutes(route => (params, animation) =>
+      router.actions.add(onFinish => {
+        const removeAllScreens = () => {
+          const cut = array => [array[array.length - 1]]
+          router.setState({ stack: cut(router.state.stack) }, onFinish)
+        }
+        router.addScreen(route, params, animation, removeAllScreens, router.state.stack.length)
+      })
+    )
+  }
+}
