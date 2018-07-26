a15e9bf48ad66c0ecb91052703c07db3f8afb158
Sergey Shpadyrev <sergeyshpadyrev@gmail.com>
2018-07-26 16:46:44 +0300

    Fix and test all examples

--- src/screen.js
+++ src/screen.js
@@ -6,11 +6,13 @@ import styles from './styles'
 export default class Screen extends React.Component {
   animateIn = () => {
     const { type, duration, easing } = Animation.withDefault(this.props.animation)
+    if (type === 'none') return Promise.resolve()
     return this.view.transitionTo(Animation.end(type), duration, easing)
   }
 
   animateOut = animation => {
     const { type, duration, easing } = Animation.withDefault(animation)
+    if (type === 'none') return Promise.resolve()
     return this.view.transitionTo(Animation.start(type), duration, easing)
   }
 
