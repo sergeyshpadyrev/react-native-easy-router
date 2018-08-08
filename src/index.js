c5ab6e7573b2f5ef6017887ef4a31ec126a2f25c
Christian Chown <christianchown@gmail.com>
2018-08-08 21:40:56 +0100

    Add onStackChange prop

--- src/index.js
+++ src/index.js
@@ -28,24 +28,37 @@ class Router extends React.Component {
       const last = this.state.stack[this.state.stack.length - 1]
       const stack = [...previous, { ...last, methods: { ...methods, id, route, params }, screen }]
 
-      this.setState({ stack }, () => screen.animateIn().then(onActionFinished))
+      this.setStack({ stack }, () => screen.animateIn().then(onActionFinished))
     }
     const screen = (
       <Screen animation={animation} animator={this.animator} key={id} ref={screenReferenceHandler}>
         <Route router={this.methods} {...params} />
       </Screen>
     )
-    this.setState({ stack: [...this.state.stack, screen] })
+    this.setStack({ stack: [...this.state.stack, screen] })
   }
 
-  componentWillMount = () => {
+  componentDidMount = () => {
+    this._isMounted = true
     this.addScreen(this.props.initialRoute, {}, { type: 'none' })
     this.hardware.subscribe()
 
     if (this.props.router) this.props.router(this.methods)
   }
 
-  componentWillUnmount = () => this.hardware.unsubscribe()
+  componentWillUnmount = () => {
+    this.hardware.unsubscribe()
+    this._isMounted = false
+  }
+
+  setStack = ({ stack }, onFinish) => {
+    if (this._isMounted) {
+      this.setState({ stack }, onFinish)
+      if (onFinish && this.props.onStackChange) {
+        this.props.onStackChange(stack.map(route => route.methods))
+      }
+    }
+  }
 
   render = () => <View style={styles.router}>{this.state.stack}</View>
 }
